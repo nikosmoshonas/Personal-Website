2c21157003d26306450ca1c78ba97933d43d2b23
nikosmoshonas <43343139+nikosmoshonas@users.noreply.github.com>
2018-09-29 19:04:06 +0200

    -
    Upload

---
+++ Meine Website/PHP-Login/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+* Database config variables
+*/
+define('DB_HOST', '160.153.146.73');
+define('DB_USER', 'nikos');
+define('DB_PASSWORD', 'Olympiakos1925');
+define('DB_DATABASE', 'myphplogin');
+
+?>
